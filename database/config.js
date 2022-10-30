e765722db69ca91f2b63747171569d215cd7ae86
WalterGuanoluisa <104540545+WalterGuanoluisa@users.noreply.github.com>
2022-10-30 03:56:06 -0500

    make it bettersss

--- database/config.js
+++ database/config.js
@@ -2,13 +2,13 @@
 //'mongoose': permite manejar de forma sencilla la comunicación entre Mongo y Node
 const mongoose=require('mongoose');
 
-const dbConnection= async() => {
+const dbConnection= () => {
 
     //try y catch: son indispensables para saber si mi aplicación falla
     try {
 
         //xq todas las 4 líneas de abajo va a retornar un Promesa puedo utilizar un await
-        await mongoose.connect(process.env.DB_CNN, {
+        mongoose.connect(process.env.DB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useCreateIndex : true
